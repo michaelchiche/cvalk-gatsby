03300771065c814f4feccade65ce240fb3281c07
Michael Chiche <michael@chichecorp.com>
2019-09-14 00:36:42 +0200

    fixes for css

--- src/components/layoutWithMenu.tsx
+++ src/components/layoutWithMenu.tsx
@@ -15,14 +15,27 @@ const cvalkLogo = require('../images/cvalk.png');
 
 const LayoutWithMenu: React.FC = ({ children }) => {
   return (
-    <div className="p-8 xl:p-32 xl:flex xl:flex-row xl:h-screen xl:m-0">
-      <div className="xl:w-1/3 mb-2 z-50 fixed xl:static">
-        <div className="w-full h-8 mb-8 xl:hidden">
-          <div className="flex mr-8 justify-between">
-            <Link to="/">
-              <img src={cvalkLogo} alt="cvalk" className="flex-shrink w-2/5" />
+    <div className="p-4 xl:p-32 xl:flex xl:flex-row xl:h-screen xl:m-0 overflow-x-hidden xl:overflow-y-auto">
+      <div className="xl:w-1/3 z-50 top-0 px-4 py-4 xl:static">
+        <div className="w-full h-auto xl:hidden">
+          <div className="flex justify-between">
+            <Link to="/" className="flex-shrink w-2/5">
+              <img src={cvalkLogo} alt="cvalk" />
             </Link>
-            <div>Menu</div>
+            <div className="flex items-center text-gray-600">
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="36"
+                  height="36"
+                  color="#979797"
+                  fill="#979797"
+                >
+                  <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
         <div className="flex flex-row">
@@ -67,7 +80,9 @@ const LayoutWithMenu: React.FC = ({ children }) => {
           </div>
         </div>
       </div>
-      <div className="xl:w-2/3 flex-grow-0 z-0 pt-20">{children}</div>
+      <div className="xl:w-2/3 flex-grow-0 z-0 xl:pt-20 flex flex-col xl:block">
+        {children}
+      </div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,10 @@ import { Link } from 'gatsby';
 const IndexPage = ({}) => (
   <LayoutWithMenu>
     <SEO title="Home" />
-    <div className="content z-0 flex flex-col h-screen justify-around items-center xl:flex-row xl:ml-20 xl:border-l-2 xl:h-full border-gray-900 ">
+    <div className="content z-0 flex flex-col h-full flex xl:justify-around items-center xl:flex-row xl:ml-20 xl:border-l-2 xl:h-full border-gray-900 ">
       <Link
         to="/copies"
-        className="bg-cover bg-center h-full w-full xl:w-2/5 relative group"
+        className="bg-cover bg-center h-64 max-h-screen xl:h-full w-full xl:w-2/5 relative group"
         style={{
           backgroundImage:
             'url(https://res.cloudinary.com/cvalk/image/upload/w_700,h_700,c_fit/v1567809543/yfsggbw5bvrbu2pnwhrj.jpg)',
@@ -23,7 +23,7 @@ const IndexPage = ({}) => (
       </Link>
       <Link
         to="/creations"
-        className="bg-cover bg-center h-full w-full xl:w-2/5 relative group mt-8"
+        className="bg-cover bg-center h-64 xl:h-full w-full xl:w-2/5 relative group mt-8"
         style={{
           backgroundImage:
             'url(https://res.cloudinary.com/cvalk/image/upload/w_700,h_700,c_fit/v1567809520/tzpjbdv0t90nolfibonc.jpg)',
